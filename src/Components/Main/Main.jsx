b271cd4703ac441754eadf306d2964dc4b83499a
navdip chothani <navdipchothani638@gmail.com>
2024-07-27 18:08:50 +0530

    gemini api integrated our website

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -11,28 +11,53 @@ function Main() {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p><span>Hello, Dev.</span></p>
-          <p>How can i help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beutiful places to see on an upcoming road trip</p>
-            <img src={assets.compass_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Briefly summarize this concept: urban planning</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Improve the readability of the following code</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        {
+          !showResult ?
+            <>
+              <div className="greet">
+                <p><span>Hello, Dev.</span></p>
+                <p>How can i help you today?</p>
+              </div>
+              <div className="cards">
+                <div className="card">
+                  <p>Suggest beutiful places to see on an upcoming road trip</p>
+                  <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Briefly summarize this concept: urban planning</p>
+                  <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Brainstorm team bonding activities for our work retreat</p>
+                  <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                  <p>Improve the readability of the following code</p>
+                  <img src={assets.code_icon} alt="" />
+                </div>
+              </div>            
+            </> : 
+            <div className='result'>
+              <div className="result-title">
+                <img src={assets.user_icon} alt="" />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+                {
+                  loading ? 
+                  <div className='loader'>
+                    <hr />
+                    <hr />
+                    <hr />
+                  </div> :
+                  <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                }
+                
+              </div>
+            </div>
+        }
+
         <div className="main-bottom">
           <div className="search-box">
             <input type="text" onChange={(e)=> setInput(e.target.value)} value = {input}  placeholder='Enter prompt here' id="" />
--- src/config/gemini.js
+++ src/config/gemini.js
@@ -41,6 +41,7 @@ import {
   
     const result = await chatSession.sendMessage(prompt);
     console.log(result.response.text());
+    return result.response.text();
   }
   
   export default run;
